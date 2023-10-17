663987b66defe5301f612574ad89df230adc84df
Giessoo <66471094+Gremlin-now@users.noreply.github.com>
2023-10-17 22:25:57 +0300

    Добавьте файлы проекта.

---
+++ CarServiceCRUDAPI/CarClass.cs
@@ -0,0 +1,10 @@
+﻿namespace CarServiceCRUDAPI
+{
+    public class CarClass
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public string ReleaseDate { get; set; }
+        public string VINCode { get; set; }
+    }
+}
