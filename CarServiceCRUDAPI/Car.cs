9b2aeeae8aadeed0d99874f5a8f31ed9da35072e
Giessoo <66471094+Gremlin-now@users.noreply.github.com>
2023-10-18 20:48:37 +0300

    CAR

---
+++ CarServiceCRUDAPI/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarServiceCRUDAPI
+{
+    public class Car
+    {
+        [Required]
+        public string Mark { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        [Range(1900, 2100)]
+        public int ReleaseYear { get; set; }
+        [Required]
+        [StringLength(17, MinimumLength = 17)]
+        public string VINCode { get; set; }
+        public Car(string mark, string model, int releaseYear, string vincode) {
+            Mark = mark;
+            Model = model;
+            ReleaseYear = releaseYear;
+            VINCode = vincode;
+        }
+    }
+}
--- CarServiceCRUDAPI/Controllers/CarController.cs
+++ CarServiceCRUDAPI/Controllers/CarController.cs
@@ -3,12 +3,69 @@
 namespace CarServiceCRUDAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]/[action]")]
     public class CarController : Controller
     {
-        public IActionResult Index()
+   
+        private static List<Car> cars = new List<Car>();
+
+        [HttpGet("cars/{id}")]
+        public Car get(int id)
         {
-            return View();
+            try
+            {
+                Response.StatusCode = 200;
+                return cars[id];
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Response.StatusCode = 404;
+                return new Car("", "", 2001, "aa000a");
+            }
+        }
+        [HttpGet("cars")]
+        public List<Car> getCars()
+        {
+            return cars;
+        }
+        [HttpPost]
+        [ActionName("addCarInList")]
+        public void post(Car car)
+        {
+            Response.StatusCode = 204;
+            if (ModelState.IsValid)
+            {
+                cars.Add(car);
+            }
+        }
+        [HttpPut]
+        [ActionName("updateCarInList")]
+        public Car put(int id, Car car)
+        {
+            try
+            {
+                Response.StatusCode = 200;
+                cars[id] = car;
+            }
+            catch
+            {
+                Response.StatusCode = 404;
+            }
+            return car;
+        }
+        [HttpDelete]
+        [ActionName("deleteCarFromList")]
+        public void delete(int id) {
+            try
+            {
+                Response.StatusCode = 204;
+                cars.RemoveAt(id);
+            }
+            catch
+            {
+                Response.StatusCode = 404;
+            }
         }
     }
 }
